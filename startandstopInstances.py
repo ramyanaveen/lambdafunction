577c25f26fe0986e56c344371278010722a0fafd
Ramya Naveen <ramyananjundappa@gamil.com>
2021-08-26 00:34:22 +0530

    edited python code file

--- startandstopInstances.py
+++ startandstopInstances.py
@@ -6,7 +6,7 @@ def startandstopinstances(event, context):
     ec2_client=aws_console.client(service_name="ec2",region_name="ap-south-1")
     for each_instances in ec2_client.describe_instances()['Reservations']:
         for each_item in each_instances['Instances']:
-            id.append(each_item['InstterranceId'])
+            id.append(each_item['InstanceId'])
     #yayy it worked
     print(id)
     response=ec2_client.stop_instances(InstanceIds=id)
