8108744d5eeff1e472a57f3d0bfeff603b4a1a52
Ramya Naveen <ramyananjundappa@gamil.com>
2021-08-25 12:16:27 +0530

    deleted aws credentials line from provider.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terraformstatefiledevelopment"
-    key    = "terraform.tfstate"
+    key    = "dev/terraform.tfstate"
     region = "ap-south-1"
-    depends_on = [aws_s3_bucket.example.terraformstatefiledevelopmentaws]
   }
 }
 
--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region                  = "ap-south-1"
-  shared_credentials_file = "C:\\Users\\nr\\.aws\\credentials"
-  profile                 = "serverless-admin"
 }
