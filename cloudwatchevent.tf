21b28dee693fa61a49c95703fd363e1391e75934
Ramya Naveen <ramyananjundappa@gamil.com>
2021-08-25 23:56:09 +0530

    adding cloudwatch event.tf to suto schedule the lambda function

---
+++ cloudwatchevent.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_event_rule" "tostartinstances"{
+    name                 = "crontostartinstances"
+    description          = "to start ip"
+    schedule_expression  = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "ec2instancestarget"{
+    rule                 = "aws_cloudwatch_event_rule.tostartinstances.crontostartinstances"
+    target_id            = "startstoppedinstances"
+    arn                  = aws_lambda_function.startandstop.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch"{
+    atatementid          = "allowexecutionfromcloudwatchevent"
+    action               = "lambda.InvokeFunction"
+    function_name        = "aws_lambda_function.startandstop.function_name"
+    principal            = "events.amazonaws.com"
+    source_arn           = "aws_cloudwatch_event_rule.tostartinstances.arn"
+}
