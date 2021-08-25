d6bfe70af15ca932e5cbc8d7bda7dae3a28b4058
Ramya Naveen <ramyananjundappa@gamil.com>
2021-08-26 00:21:55 +0530

    removed profile_name from python code file

--- startandstopInstances.py
+++ startandstopInstances.py
@@ -2,7 +2,7 @@ import boto3
 id=[]
 #hope it works
 def startandstopinstances(event, context):
-    aws_console=boto3.session.Session(profile_name="serverless-admin")
+    aws_console=boto3.session.Session()
     ec2_client=aws_console.client(service_name="ec2",region_name="ap-south-1")
     for each_instances in ec2_client.describe_instances()['Reservations']:
         for each_item in each_instances['Instances']:
