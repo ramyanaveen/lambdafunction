c506e2b3608b14d927e4f918db3de38665f02f85
Ramya Naveen <ramyananjundappa@gamil.com>
2021-08-26 00:00:26 +0530

    corrected a line in cloudwatchevent.tf

--- cloudwatchevent.tf
+++ cloudwatchevent.tf
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_event_target" "ec2instancestarget"{
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch"{
-    statementid          = "allowexecutionfromcloudwatchevent"
+    statement_id          = "allowexecutionfromcloudwatchevent"
     action               = "lambda.InvokeFunction"
     function_name        = "aws_lambda_function.startandstop.function_name"
     principal            = "events.amazonaws.com"
